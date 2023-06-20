503a7c9b20a1f1ed2adb512eadd48e3b832e59ae
Phancy-Andrew <loiskui485@gmail.com>
2023-06-20 18:54:41 +0000

    first commit

--- frontend/src/MainPage.jsx
+++ frontend/src/MainPage.jsx
@@ -6,32 +6,89 @@ import "./App.css";
 import { useState } from "react";
 import tt from "@tomtom-international/web-sdk-maps";
 import "@tomtom-international/web-sdk-maps/dist/maps.css";
+import ttapi from "@tomtom-international/web-sdk-services";
 
 function MainPage() {
   const mapElement = useRef();
+  const [latitude, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
 
   const [map, setMap] = useState({});
+  navigator.geolocation.getCurrentPosition((position) => {
+    setLatitude(position.coords.latitude);
+    setLongitude(position.coords.longitude);
+  });
 
-  const [latitude, setLatitude] = useState(6.6885);
-  const [longitude, setLongitude] = useState(-1.6244);
+  const convertToPoints = (lnglat) => {
+    return {
+      point: {
+        latitude: lnglat.lat,
+        longitude: lnglat.lng,
+      },
+    };
+  };
+
+  const drawRoute = (geoJson, map) => {
+    console.log("the line is drawn");
+    if (map.getLayer("route")) {
+      map.removeLayer("route");
+      map.removeSource("route");
+    }
+    map.addLayer({
+      id: "route",
+      type: "line",
+      source: { type: "geojson", data: geoJson },
+      paint: { "line-color": "gray", "line-width": 2 },
+    });
+  };
+
+  const addDeliveryMarker = (lnglat, map) => {
+    const element = document.createElement("div");
+    element.classList.add(
+      "bg-cover",
+      "h-[8px]",
+      "w-[8px]",
+      "bg-[blue]",
+      "border",
+      "rounded-lg",
+      "border-solid-gray"
+    );
+    new tt.Marker({ element: element }).setLngLat(lnglat).addTo(map);
+  };
+
   useEffect(() => {
+    const destinations = [];
+
+    const origin = {
+      lat: latitude,
+      lng: longitude,
+    };
+
     let map = tt.map({
       key: "tGs7nOkNWSZKSWIBx3Ln2m7ZM4QN26ix",
       container: mapElement.current,
       stylesVisibility: { trafficIncidents: true, trafficFlow: true },
       center: [longitude, latitude],
-      zoom: 10,
+      zoom: 14,
     });
+
     setMap(map);
+
     const addMarker = () => {
+      const popupOffset = {
+        bottom: [0, -25],
+      };
+      const popup = new tt.Popup({ offset: popupOffset }).setHTML(
+        "this is you"
+      );
       const element = document.createElement("div");
       element.classList.add(
         "bg-cover",
         "h-[22px]",
-        "w-[22px]",
-        "bg-yellow",
+        "w-[10px]",
+        "bg-[yellow]",
         "border",
-        "rounded-lg",
+        "rounded-2xl",
         "border-solid-gray"
       );
       const marker = new tt.Marker({
@@ -45,8 +102,63 @@ function MainPage() {
         setLongitude(lnglat.lng);
         setLatitude(lnglat.lat);
       });
+      marker.setPopup(popup).togglePopup();
     };
+
     addMarker();
+
+    const sortDestinations = (locations) => {
+      const pointsForDestinations = locations.map((destination) => {
+        return convertToPoints(destination);
+      });
+
+      const callParameters = {
+        key: "tGs7nOkNWSZKSWIBx3Ln2m7ZM4QN26ix",
+        destinations: pointsForDestinations,
+        origins: [convertToPoints(origin)],
+      };
+      return new Promise((resolve, reject) => {
+        ttapi.services
+          .matrixRouting(callParameters)
+          .then((matrixAPIResults) => {
+            const results = matrixAPIResults.matrix[0];
+            const resultsArray = results.map((result, index) => {
+              return {
+                location: locations[index],
+                drivingtime: result.response.routeSummary.travelTimeInSeconds,
+              };
+            });
+            resultsArray.sort((a, b) => {
+              a.drivingtime - b.drivingtime;
+            });
+            const sortedLocations = resultsArray.map((result) => {
+              return result.location;
+            });
+            resolve(sortedLocations);
+          });
+      });
+    };
+    const recalculatedRoutes = () => {
+      // console.log("calculated");
+      sortDestinations(destinations).then((sorted) => {
+        sorted.unshift(origin);
+        ttapi.services
+          .calculateRoute({
+            key: "tGs7nOkNWSZKSWIBx3Ln2m7ZM4QN26ix",
+            locations: sorted,
+          })
+          .then((routeData) => {
+            const geoJson = routeData.toGeoJson();
+            drawRoute(geoJson, map);
+          });
+      });
+    };
+
+    map.on("click", (e) => {
+      destinations.push(e.lngLat);
+      addDeliveryMarker(e.lngLat, map);
+      recalculatedRoutes();
+    });
     return () => map.remove();
   }, [latitude, longitude]);
   return (
@@ -57,11 +169,14 @@ function MainPage() {
             <h1 className=" font-semibold justify-start text-2xl">BinBuddy</h1>
             <SideBar />
           </div>
-          <div className="flex h-[520px] border justify-end items-baseline">
-            <div ref={mapElement} className="h-[400px]" />
+          <div className=" h-[600px]">
+            {map && (
+              <div>
+                <div ref={mapElement} className="h-[600px]" />
+              </div>
+            )}
             <div>
-              <h1>Where to?</h1>
-              <input
+              {/* <input
                 type="text"
                 id="longitude"
                 placeholder="Put in longitude"
@@ -75,8 +190,8 @@ function MainPage() {
                 placeholder="Put in latitude"
                 onChange={(e) => {
                   setLatitude(e.target.value);
-                }}
-              />
+                }} */}
+              {/* /> */}
             </div>
           </div>
           {/* <button className="fixed z-90 bottom-5  right-3 bg-g3 w-10 h-10 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-g2 duration-300"></button> */}
