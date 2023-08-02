97c5d10a1b391f3e20350d49da350030a18b0aab
Phancy-Andrew <loiskui485@gmail.com>
2023-08-02 20:18:07 +0000

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3001, // Change this to your desired port
-  },
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: false,
+        skipWaiting: true
+      },
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Youvote",
+        short_name: "Youvote",
+        description: "Voting web app for youvote ",
+        theme_color: "#ffffff",
+        start_url: "/",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ]
+});
