32a5f7fd155894c09554262202246f302d4d2500
Phancy-Andrew <loiskui485@gmail.com>
2023-08-02 13:24:39 +0000

    updates

--- src/SignUpPage.jsx
+++ src/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { adminClient } from "../apiEndpoints/endpoints.js";
 
