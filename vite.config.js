6fd67c2d944c0626b2ac72f2216f1e6beb9354a0
Phancy-Andrew <loiskui485@gmail.com>
2023-08-03 00:09:41 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
@@ -17,24 +19,24 @@ export default defineConfig({
       },
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
       manifest: {
-        name: "Youvote",
-        short_name: "Youvote",
-        description: "Voting web app for youvote ",
+        name: "BinBuddy-driver",
+        short_name: "BinBuddy",
+        description: "a waste management app ",
         theme_color: "#ffffff",
         start_url: "/",
         icons: [
           {
-            src: "logo192.png",
+            src: "splash2.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "logo512.png",
+            src: "splash2.png",
             sizes: "512x512",
             type: "image/png",
           },
           {
-            src: "logo512.png",
+            src: "splash2.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
