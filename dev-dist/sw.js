8d5d026d637afa33093ff0dd6bb0049a35ba900d
Phancy-Andrew <loiskui485@gmail.com>
2023-08-03 00:42:14 +0000

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ufg4q1uu8vg"
+    "revision": "0.6qbv5rsc1b"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
